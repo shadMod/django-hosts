ae678a3688ebb6333aaf32a0d52299e0bd6cf117
mattia allegro <mattia.allegro@yahoo.it>
2023-05-06 15:18:41 +0200

    clean init

--- django_hosts/__init__.py
+++ django_hosts/__init__.py
@@ -1,12 +1,11 @@
 # flake8: noqa
-import pkg_resources
+from django_hosts.defaults import patterns, host
+from django_hosts.resolvers import (
+    reverse,
+    reverse_lazy,
+    reverse_host,
+    reverse_host_lazy,
+)
 
-try:  # pragma: no cover
-    from django_hosts.defaults import patterns, host
-    from django_hosts.resolvers import (reverse, reverse_lazy,
-                                        reverse_host, reverse_host_lazy)
-except ImportError:  # pragma: no cover
-    pass
-
-__version__ = pkg_resources.get_distribution('django-hosts').version
-__author__ = 'Jazzband members (https://jazzband.co/)'
+__version__ = "5.2"
+__author__ = "Jazzband members (https://jazzband.co/)"
